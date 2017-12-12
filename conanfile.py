19094c3f0ff0e86a91e99eb1f57a87343a5e6cac
danimtb <danimanzaneque@gmail.com>
2017-12-12 12:44:13 +0100

    Updated conanfile

--- conanfile.py
+++ conanfile.py
@@ -10,56 +10,54 @@ class OpenBLASConan(ConanFile):
     name = "OpenBLAS"
     version = "0.2.20"
     url = "https://github.com/xianyi/OpenBLAS"
+    homepage = "http://www.openblas.net/"
     description = "OpenBLAS is an optimized BLAS library based on GotoBLAS2 1.13 BSD version."
     license = "https://github.com/xianyi/OpenBLAS/blob/master/LICENSE"
     exports_sources = ["CMakeLists.txt", "LICENSE"]
     settings = "os", "arch", "compiler", "build_type"
-    options = {"NO_SHARED": ["1", "0"],
-               "USE_MASS": ["1", "0"],  # Compile with MASS Support on Power CPU (Optional dependency)
-               "USE_OPENMP": ["1", "0"],
-               "NO_LAPACKE": ["1", "0"]
+    options = {"shared": [True, False],
+               "use_mass": [True, False],
+               "use_openmp": [True, False],
+               "no_lapacke": [True, False]
               }
-    default_options = "NO_SHARED=0", "USE_MASS=0", "USE_OPENMP=0", "NO_LAPACKE=0"
+    default_options = "shared=True", "use_mass=False", "use_openmp=False", "no_lapacke=False"
 
     def get_make_arch(self):
-        if self.settings.arch == "x86":
-            return "32"
-        else:
-            return "64"
+        return "32" if self.settings.arch == "x86" else "64"
 
     def get_make_build_type_debug(self):
-        if self.settings.build_type == "Release":
-            return "0"
-        else:
-            return "1"
+        return "0" if self.settings.build_type == "Release" else "1"
+
+    def get_make_option_value(self, option):
+        return "1" if option else "0"
 
     def source(self):
         source_url = "https://sourceforge.net/projects/openblas"
         file_name = ("{0} {1} version".format(self.name, self.version))
         tools.get("{0}/files/v{1}/{2}.tar.gz".format(source_url, self.version, file_name))
         os.rename(glob("xianyi-OpenBLAS-*")[0], "sources")
-        #Rename to "sources" is a convention to simplify later steps
 
     def build(self):
         make_program = os.getenv("CONAN_MAKE_PROGRAM", "make")
 
         if tools.which(make_program):
-            make_options = "DEBUG={0} NO_SHARED={1} BINARY={2} NO_LAPACKE={3} USE_MASS={4} USE_OPENMP={5}".format(self.get_make_build_type_debug(), self.options.NO_SHARED, self.get_make_arch(), self.options.NO_LAPACKE, self.options.USE_MASS, self.options.USE_OPENMP)
+            make_options = "DEBUG={0} NO_SHARED={1} BINARY={2} NO_LAPACKE={3} USE_MASS={4} USE_OPENMP={5}".format(
+                self.get_make_build_type_debug(),
+                self.get_make_option_value(self.options.shared),
+                self.get_make_arch(),
+                self.get_make_option_value(self.options.no_lapacke),
+                self.get_make_option_value(self.options.use_mass),
+                self.get_make_option_value(self.options.use_openmp))
             self.run("cd sources && make %s" % make_options)
         else:
             cmake = CMake(self)
-
-            if (self.options.NO_SHARED == "1"):
-                cmake.definitions["BUILD_SHARED_LIBS"] = "FALSE"
-            else:
-                cmake.definitions["BUILD_SHARED_LIBS"] = "TRUE"
-
             cmake.configure(source_dir="sources")
             cmake.build()
 
     def package(self):
         with tools.chdir("sources"):
-            self.copy(pattern="LICENSE")
+            self.copy(pattern="LICENSE", dst="licenses", src="sources",
+                ignore_case=True, keep_path=False)
 
             if self.settings.compiler == "Visual Studio":
                 self.copy(pattern="*.h", dst="include", src=".")
